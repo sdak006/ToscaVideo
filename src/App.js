c136844cf4b901cae453f67331b19ddc40288a0f
Scott Dakers <sdak006@gmail.com>
2020-08-09 21:29:19 -0500

    foo

--- src/App.js
+++ src/App.js
@@ -4,10 +4,29 @@ import ReactPlayer from 'react-player'
 import Data from './data.json'
 
 class App extends Component {
+
+  state = {
+    duration: 0,
+    playedSeconds: 0
+  }
+
+
   ref = player => {
     this.player = player
   }
 
+  handleProgress = state => {
+    console.log('onProgress', state)
+    // We only want to update time slider if we are not currently seeking
+    if (!this.state.seeking) {
+      this.setState(state)
+    }
+  }
+
+  handleDuration = (duration) => {
+    console.log('onDuration', duration)
+    this.setState({ duration })
+  }
 
   render() {
     const config = {
@@ -22,17 +41,26 @@ class App extends Component {
             {
               Data.map(
                 (d, index) =>
+                {
+                  var startTime=Math.floor(parseFloat(d.TimeStamp / 1000));
+                  var endTime=Math.floor(parseFloat(d.EndTime / 1000));
+                  return(
                   <div
                     key={index}
                     onClick={
                       () => {
-                        console.log(Math.floor(parseFloat(d.TimeStamp / 1000)));
-                        this.player.seekTo(Math.floor(parseFloat(d.TimeStamp / 1000)), 'seconds')
+                        console.log(startTime);
+                        this.player.seekTo(startTime, 'seconds')
                       }
                     }
-                    style={{ borderWidth: 5, borderColor: 'white', margin: '5px' }}>
+                    style={{
+                      backgroundColor: this.state.playedSeconds >= startTime && this.state.playedSeconds <= endTime ? 'red':'#282c34',
+                      margin: '5px'
+                    }}>
                     {d.Name}
-                  </div>)
+                  </div>
+                  )
+                })
             }
           </div>
           <ReactPlayer
@@ -45,6 +73,8 @@ class App extends Component {
                 hlsOptions: config
               }
             }}
+            onProgress={this.handleProgress}
+            onDuration={this.handleDuration}
             url='https://toscavideo-output.s3-us-west-1.amazonaws.com/2cb5c76c-a76e-4aa8-8367-1bef9ef47b36.m3u8'
           />
         </header>
